62efe348d63988804536df5e06589b45344cf2b0
wilhan <Wilhan Pereira>
2022-11-02 22:34:21 -0300

    adicão da logo e a alteração da navbar

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
+var cabecalho = document.getElementById('cabecalho');
+var navgationHeader = document.getElementById('navigation_header');
+var showSidebar =false;
 
-
-function chamarPgForm(){
-  window.location.href="../checklist/checklist.html";
+function abrirNavBar(){
+  showSidebar = !showSidebar;
+  
+  if(showSidebar){
+    navgationHeader.style.marginLeft ='-10vw';
+    navgationHeader.style.animationName='showSiidebar';
+   } else {
+    navgationHeader.style.marginLeft ='-100vw';
+    navgationHeader.style.animationName='';
+   }
 }
+
+window.addEventListener('resize', function(event){
+  if(window.innerWidth > 768 && showSidebar){
+    abrirNavBar();
+  }
+})
